3853bf2878d8d953254ade32b7e3f7898e91eb94
Ahmed <ahmed@example.com>
2026-03-03 01:04:56 +0530

    Update CORS to allow all origins

--- main.py
+++ main.py
@@ -13,16 +13,9 @@ from routes import auth
 app = FastAPI(title="HealthSecure API")
 
 # CORS (ALLOW YOUR REACT APP)
-origins = [
-    "https://healthsecuredashboard-beta.vercel.app",
-    "https://healthsecuredashboard-gb1wpfl1t-ahmedd-ais-projects.vercel.app",
-    "http://localhost:5173",
-    "http://localhost:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
